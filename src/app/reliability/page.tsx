0368c3254a04284b2b96d770e6db81fa0f978fc3
Lawrence Caulker <locaulker@gmail.com>
2023-12-12 19:09:24 -0500

    added and styled navbar

--- src/app/reliability/page.tsx
+++ src/app/reliability/page.tsx
@@ -1,3 +1,12 @@
-export default function ReliabilityPage() {
-  return <div>Reliability Page!</div>
+import reliabilityImg from "public/reliability.jpg"
+import Hero from "@/components/Hero"
+
+export default function Reliability() {
+  return (
+    <Hero
+      imgData={reliabilityImg}
+      imgAlt="Welding"
+      title="Super high reliability Hosting"
+    />
+  )
 }
--- src/app/scale/page.tsx
+++ src/app/scale/page.tsx
@@ -1,3 +1,12 @@
+import scaleImg from "public/scale.jpg"
+import Hero from "@/components/Hero"
+
 export default function ScalePage() {
-  return <div>Scale Page!</div>
+  return (
+    <Hero
+      imgData={scaleImg}
+      imgAlt="Steel Factory"
+      title="Scale your app to infinity"
+    />
+  )
 }
